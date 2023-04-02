95706065d62527b94008f8f1c27ff287fa48118a
Takuzenn <l2579904133@gmail.com>
2023-04-02 06:27:54 -0500

    add loading

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,18 +1,21 @@
 import {Link} from "react-router-dom";
 import {useContext} from "react";
 import {UserContext} from "./UserContext.jsx";
+import icon from "./assets/icon.svg"
 
 export default function Header() {
   const {user} = useContext(UserContext);
   return (
     <header className="flex justify-between">
       <Link to={'/'} className="flex items-center gap-1">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 -rotate-90">
+        {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 -rotate-90">
           <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>
+        </svg> */}
+        <img src={icon} className="w-8 h-8"  alt="Icon" />
+
         <span className="font-bold text-xl">Stuniverse</span>
       </Link>
-      <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
+      {/* <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
         <div>Anywhere</div>
         <div className="border-l border-gray-300"></div>
         <div>Any week</div>
@@ -23,7 +26,7 @@ export default function Header() {
             <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
           </svg>
         </button>
-      </div>
+      </div> */}
       <Link to={user?'/account':'/login'} className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 ">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,31 +1,45 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Image from "../Image.jsx";
 
 export default function IndexPage() {
-  const [places,setPlaces] = useState([]);
+  const [places, setPlaces] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     axios.get('/api/places').then(response => {
       setPlaces(response.data);
+      setIsLoading(false);
     });
   }, []);
+
   return (
-    <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
-      {places.length > 0 && places.map(place => (
-        <Link to={'/place/'+place._id}>
-          <div className="bg-gray-500 mb-2 rounded-2xl flex">
-            {place.photos?.[0] && (
-              <Image className="rounded-2xl object-cover aspect-square" src={place.photos?.[0]} alt=""/>
-            )}
-          </div>
-          <h2 className="font-bold">{place.address}</h2>
-          <h3 className="text-sm text-gray-500">{place.title}</h3>
-          <div className="mt-1">
-            <span className="font-bold">${place.price}</span> per night
-          </div>
-        </Link>
-      ))}
+    <div className="mt-8">
+      {isLoading ? (
+        <div className="flex justify-center items-center h-screen">
+          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+        </div>
+      ) : (
+        <div className="grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
+          {places.map(place => (
+            <Link to={`/place/${place._id}`} key={place._id}>
+              <div className="bg-gray-500 mb-2 rounded-2xl flex">
+                {place.photos?.[0] && (
+                  <Image className="rounded-2xl object-cover aspect-square" src={place.photos?.[0]} alt=""/>
+                )}
+              </div>
+              <div className="">
+                <h2 className="font-bold">{place.address}</h2>
+                <h3 className="text-sm text-gray-500">{place.title.length > 25 ? place.title.slice(0, 25) + "..." : place.title}</h3>
+                <div className="mt-1">
+                  <span className="font-bold">${place.price}</span> per night
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
