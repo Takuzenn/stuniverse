e4ef1c2241b3d3f6c592ee27d5cbffc0e7f102c9
Takuzenn <l2579904133@gmail.com>
2023-04-02 02:13:03 -0500

    modified proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
