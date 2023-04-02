597ff5680db6a09afc112aaba9db3bc2ed0a3cb6
Takuzenn <l2579904133@gmail.com>
2023-04-02 02:17:05 -0500

    modified vite.config's bug on url

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000',
+        target: process.env.VITE_API_BASE_URL || 'http://localhost:4000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
