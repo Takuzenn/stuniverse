ad04438c06ca0b8f519b55fb13938183221afe54
Takuzenn <l2579904133@gmail.com>
2023-04-02 02:48:42 -0500

    changed cors' orign url

--- api/index.js
+++ api/index.js
@@ -26,7 +26,8 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:5173',
+  origin: 'https://stuniverse.vercel.app',
+  // origin: 'http://localhost:5173',
 }));
 
 async function uploadToS3(path, originalFilename, mimetype) {
