3e305b4016943395e6f20ff8f66d10ff0154224b
Takuzenn <l2579904133@gmail.com>
2023-04-02 00:56:40 -0500

    first commit

--- api/index.js
+++ api/index.js
@@ -13,27 +13,28 @@ const multer = require('multer');
 const fs = require('fs');
 const mime = require('mime-types');
 
+
 require('dotenv').config();
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
-const bucket = 'dawid-booking-app';
+const bucket = 'stuniverse';
 
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
   credentials: true,
-  origin: 'http://127.0.0.1:5173',
+  origin: 'http://localhost:5173',
 }));
 
 async function uploadToS3(path, originalFilename, mimetype) {
   const client = new S3Client({
     region: 'us-east-1',
     credentials: {
-      accessKeyId: process.env.S3_ACCESS_KEY,
-      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+      accessKeyId: 'AKIA3W3DPPU5HEDXWY76',
+      secretAccessKey: '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',
     },
   });
   const parts = originalFilename.split('.');
@@ -58,14 +59,23 @@ function getUserDataFromReq(req) {
   });
 }
 
+console.log(process.env.MONGO_URL)
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb+srv://l2579904133:Ll352513@cluster0.wedn4mq.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to MongoDB successfully.');
+});
+
 app.get('/api/test', (req,res) => {
-  mongoose.connect(process.env.MONGO_URL);
+  mongoose.connect('mongodb+srv://l2579904133:Ll352513@cluster0.wedn4mq.mongodb.net/?retryWrites=true&w=majority');
   res.json('test ok');
 });
 
 app.post('/api/register', async (req,res) => {
-  mongoose.connect(process.env.MONGO_URL);
+  mongoose.connect('mongodb+srv://l2579904133:Ll352513@cluster0.wedn4mq.mongodb.net/?retryWrites=true&w=majority');
   const {name,email,password} = req.body;
+  res.json({name,email,password})
+  res.json(userDoc);
 
   try {
     const userDoc = await User.create({
@@ -81,7 +91,7 @@ app.post('/api/register', async (req,res) => {
 });
 
 app.post('/api/login', async (req,res) => {
-  mongoose.connect(process.env.MONGO_URL);
+  mongoose.connect('mongodb+srv://l2579904133:Ll352513@cluster0.wedn4mq.mongodb.net/?retryWrites=true&w=majority');
   const {email,password} = req.body;
   const userDoc = await User.findOne({email});
   if (userDoc) {
@@ -103,7 +113,7 @@ app.post('/api/login', async (req,res) => {
 });
 
 app.get('/api/profile', (req,res) => {
-  mongoose.connect(process.env.MONGO_URL);
+  mongoose.connect('mongodb+srv://l2579904133:Ll352513@cluster0.wedn4mq.mongodb.net/?retryWrites=true&w=majority');
   const {token} = req.cookies;
   if (token) {
     jwt.verify(token, jwtSecret, {}, async (err, userData) => {
@@ -123,6 +133,10 @@ app.post('/api/logout', (req,res) => {
 
 app.post('/api/upload-by-link', async (req,res) => {
   const {link} = req.body;
+  console.log(link)
+  if (!link) {
+    return res.status(400).json({ error: 'The link parameter is required' });
+  }
   const newName = 'photo' + Date.now() + '.jpg';
   await imageDownloader.image({
     url: link,
@@ -135,6 +149,7 @@ app.post('/api/upload-by-link', async (req,res) => {
 const photosMiddleware = multer({dest:'/tmp'});
 app.post('/api/upload', photosMiddleware.array('photos', 100), async (req,res) => {
   const uploadedFiles = [];
+  console.log(req.files);
   for (let i = 0; i < req.files.length; i++) {
     const {path,originalname,mimetype} = req.files[i];
     const url = await uploadToS3(path, originalname, mimetype);
@@ -144,7 +159,7 @@ app.post('/api/upload', photosMiddleware.array('photos', 100), async (req,res) =
 });
 
 app.post('/api/places', (req,res) => {
-  mongoose.connect(process.env.MONGO_URL);
+  mongoose.connect('mongodb+srv://l2579904133:Ll352513@cluster0.wedn4mq.mongodb.net/?retryWrites=true&w=majority');
   const {token} = req.cookies;
   const {
     title,address,addedPhotos,description,price,
@@ -162,7 +177,7 @@ app.post('/api/places', (req,res) => {
 });
 
 app.get('/api/user-places', (req,res) => {
-  mongoose.connect(process.env.MONGO_URL);
+  mongoose.connect('mongodb+srv://l2579904133:Ll352513@cluster0.wedn4mq.mongodb.net/?retryWrites=true&w=majority');
   const {token} = req.cookies;
   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
     const {id} = userData;
@@ -171,13 +186,13 @@ app.get('/api/user-places', (req,res) => {
 });
 
 app.get('/api/places/:id', async (req,res) => {
-  mongoose.connect(process.env.MONGO_URL);
+  mongoose.connect('mongodb+srv://l2579904133:Ll352513@cluster0.wedn4mq.mongodb.net/?retryWrites=true&w=majority');
   const {id} = req.params;
   res.json(await Place.findById(id));
 });
 
 app.put('/api/places', async (req,res) => {
-  mongoose.connect(process.env.MONGO_URL);
+  mongoose.connect('mongodb+srv://l2579904133:Ll352513@cluster0.wedn4mq.mongodb.net/?retryWrites=true&w=majority');
   const {token} = req.cookies;
   const {
     id, title,address,addedPhotos,description,
@@ -198,12 +213,12 @@ app.put('/api/places', async (req,res) => {
 });
 
 app.get('/api/places', async (req,res) => {
-  mongoose.connect(process.env.MONGO_URL);
+  mongoose.connect('mongodb+srv://l2579904133:Ll352513@cluster0.wedn4mq.mongodb.net/?retryWrites=true&w=majority');
   res.json( await Place.find() );
 });
 
 app.post('/api/bookings', async (req, res) => {
-  mongoose.connect(process.env.MONGO_URL);
+  mongoose.connect('mongodb+srv://l2579904133:Ll352513@cluster0.wedn4mq.mongodb.net/?retryWrites=true&w=majority');
   const userData = await getUserDataFromReq(req);
   const {
     place,checkIn,checkOut,numberOfGuests,name,phone,price,
@@ -221,7 +236,7 @@ app.post('/api/bookings', async (req, res) => {
 
 
 app.get('/api/bookings', async (req,res) => {
-  mongoose.connect(process.env.MONGO_URL);
+  mongoose.connect('mongodb+srv://l2579904133:Ll352513@cluster0.wedn4mq.mongodb.net/?retryWrites=true&w=majority');
   const userData = await getUserDataFromReq(req);
   res.json( await Booking.find({user:userData.id}).populate('place') );
 });
